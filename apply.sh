a42f28734f47bc37d530a5b4a7ede966602a69f5
pascal simon <ps@s-it.ch>
2018-06-07 07:59:34 +0200

    update apply script

--- apply.sh
+++ apply.sh
@@ -1,11 +1,25 @@
-sudo apt install git
+SRC_BASE_DIR=~/git/psunix
 
-cd ~
-mkdir -p git/psunix
-cd ~/git/psunix
-git clone https://github.com/psunix/dp.git desktop-provisioning
-cd desktop-provisioning
-./bin/install_ansible.sh
+if ! hash git >/dev/null; then
+  sudo apt install git
+fi
+
+mkdir -p $SRC_BASE_DIR
+cd $SRC_BASE_DIR
+
+if ! hash ansible-playbook >/dev/null; then
+  ./bin/install_ansible.sh
+fi
+
+if [ ! -d "$SRC_BASE_DIR/dp" ]; then
+  git clone https://github.com/psunix/dp.git
+  cd dp
+else
+  cd dp
+  git pull
+fi
+
+git submodule update --init
 
 ansible-playbook ansible/desktop.yml --ask-become-pass
 
